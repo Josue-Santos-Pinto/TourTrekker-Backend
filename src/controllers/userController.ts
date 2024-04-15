3073607f892ca076a3002e96b65253ebd157ac58
Josue-Santos-Pinto <js.pinto55@gmail.com>
2024-04-15 17:13:27 -0300

    installed and configured JWT

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 import { createUser } from "../services/user";
+import JWT from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 
 export const userController = {
     register: async (req: Request, res: Response) => {
@@ -8,7 +13,13 @@ export const userController = {
         if(email && password){
             const user = await createUser({email, password})
             if(user){
-                res.status(201).json({user})
+                const token = JWT.sign(
+                    {id: user.id, email: user.email},
+                    process.env.JWT_SECRET_KEY as string,
+                    {expiresIn: '2h'}
+                )
+                res.status(201)
+                .json({status: true, user, token})
             } 
         } else {
             res.json({error: 'Preencha todos os campos'})
@@ -21,6 +32,7 @@ export const userController = {
         if(email && password){
             const user = await createUser({email, password})
             if(user){
+               
                 res.status(201).json({user})
             } 
         } else {
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import JWT from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const Auth = {
     private: (req: Request, res: Response, next: NextFunction) => {
-        let success = true
+        let success = false
+
+        if(req.headers.authorization){
+            const [authType, token] = req.headers.authorization.split(' ')
+            if(authType == 'Bearer'){
+                try {
+                    JWT.verify(token, process.env.JWT_SECRET_KEY as string)
+                    success = true
+                } catch(error) {
+                    
+                }
+                
+            }
+        }
 
         if(success){
             next()
